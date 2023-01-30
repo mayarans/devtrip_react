31e9ec6125408d6516705df304777899230df78e
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2023-01-30 17:13:37 -0300

    add pop up funcionality

--- public/behavior.js
+++ public/behavior.js
@@ -141,3 +141,25 @@ const buttonChangeLoginToSignUpModal = document.querySelector("#signup")
 
 buttonChangeSignUpToLoginModal.addEventListener("click", openLoginModal)
 buttonChangeLoginToSignUpModal.addEventListener("click", openSignUpModal)
+
+/* Abrindo pop up */
+
+const getGoogle = document.querySelectorAll(".login-google")
+const getFacebook = document.querySelectorAll(".login-facebook")
+
+getGoogle.forEach((option, i) => {
+  getGoogle[i].addEventListener("click", () => {
+    window.open(
+      "https://accounts.google.com/v3/signin/identifier?dsh=S135106958%3A1667686411265880&authuser=0&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fclient%3Dopera-gx%26q%3Dgoogle%2Blogin%26sourceid%3Dopera%26ie%3DUTF-8%26oe%3DUTF-8&ec=GAlAAQ&hl=pt-BR&flowName=GlifWebSignIn&flowEntry=AddSession",
+      "Fazer login com conta Google",
+      "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, width=600, height=550, top=150, left=600"
+    )
+  })
+  getFacebook[i].addEventListener("click", () => {
+    window.open(
+      "https://pt-br.facebook.com/",
+      "Fazer login com o Facebook",
+      "toolbar=no,location=no,directories=no,status=no, menubar=no,scrollbars=yes,resizable=no,width=600, height=550,top=150,left=600"
+    )
+  })
+})
